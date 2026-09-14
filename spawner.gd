155564c43c8089a27b8cebe93fc35c908c56217b
Varun Kapoor <vk2864@nyu.edu>
2026-09-13 21:51:03 -0400

    scaling for spawner timings

--- spawner.gd
+++ spawner.gd
@@ -5,26 +5,31 @@ extends Node2D
 @export var speed := 400.0
 
 var timer: Timer
-var increment_speed = 0
-var MIN_SPEED = 0.1
-var MAX_SPEED = 1
+var no_of_spawn_in_level = 0
+var min_interval = 0.2
+var max_interval = 1
+var no_of_spawn_till_new_level = 10
 
 func _ready() -> void:
 	timer = Timer.new()
 	timer.one_shot = true
 	timer.timeout.connect(spawn)
 	add_child(timer)
-	timer.start(randf_range(MIN_SPEED, MAX_SPEED))
+	timer.start(randf_range(min_interval, max_interval))
 
 func spawn() -> void:
 	var xo : Node2D = XO_scene.pick_random().instantiate()
 	xo.global_position = global_position
 	xo.speed = speed
 	get_parent().add_child(xo)
-	increment_speed += 1
-	if increment_speed > 10:
+	no_of_spawn_in_level += 1
+	if no_of_spawn_in_level > no_of_spawn_till_new_level:
 		speed += 200
-		increment_speed = 0
+		no_of_spawn_in_level = 0
+		if min_interval > 0.1:
+			min_interval -= 0.1
+		if max_interval > 0.1:
+			max_interval -= 0.1
 		timer.start(5)
 	else:
-		timer.start(randf_range(MIN_SPEED, MAX_SPEED))
+		timer.start(randf_range(min_interval, max_interval))
