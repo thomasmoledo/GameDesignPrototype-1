81332d6c70d3735e87b57c8886ff2fce203f87fd
Varun Kapoor <vk2864@nyu.edu>
2026-09-13 20:38:17 -0400

    spawner mods

--- spawner.gd
+++ spawner.gd
@@ -6,21 +6,25 @@ extends Node2D
 
 var timer: Timer
 var increment_speed = 0
+var MIN_SPEED = 0.1
+var MAX_SPEED = 1
 
 func _ready() -> void:
 	timer = Timer.new()
 	timer.one_shot = true
 	timer.timeout.connect(spawn)
 	add_child(timer)
-	timer.start(randf_range(0.5, 2))
+	timer.start(randf_range(MIN_SPEED, MAX_SPEED))
 
 func spawn() -> void:
 	var xo : Node2D = XO_scene.pick_random().instantiate()
 	xo.global_position = global_position
 	xo.speed = speed
 	get_parent().add_child(xo)
-	timer.start(randf_range(0.5, 2))
 	increment_speed += 1
 	if increment_speed > 10:
 		speed += 200
 		increment_speed = 0
+		timer.start(5)
+	else:
+		timer.start(randf_range(MIN_SPEED, MAX_SPEED))
