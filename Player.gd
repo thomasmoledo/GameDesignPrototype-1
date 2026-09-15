76c8c9bc72badc65b137534031a744e72024b26b
Varun Kapoor <vk2864@nyu.edu>
2026-09-14 22:33:54 -0400

    audio addition and game restart

--- Player.gd
+++ Player.gd
@@ -6,6 +6,7 @@ extends Area2D
 @onready var texture_progress_bar: TextureProgressBar = $"../CanvasLayer/TextureProgressBar"
 @onready var label: Label = $"../CanvasLayer/Label"
 
+var end_scene = false
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
@@ -18,12 +19,16 @@ func _process(delta: float) -> void:
 
 func _input(event: InputEvent) -> void:
 	if event.is_action_pressed("Click"):
+		if end_scene == true:
+			get_tree().reload_current_scene()
 		if curr_symbol:
 			if !curr_symbol.is_circle():
 				lose_health(25)
+				get_node("failed_audio").play()
 			elif curr_symbol.is_circle():
 				gain_health(2)
 				points += 1
+				get_node("success_audio").play()
 			curr_symbol.queue_free()
 			curr_symbol = null
 			texture_progress_bar.value = player_health
@@ -31,7 +36,6 @@ func _input(event: InputEvent) -> void:
 
 func _on_area_entered(area: Area2D) -> void:
 	curr_symbol = area
-
 
 func _on_area_exited(area: Area2D) -> void:
 	curr_symbol = null
@@ -45,8 +49,11 @@ func gain_health(health: int) -> void:
 func lose_health(health: int) -> void:
 	if player_health - health < 0:
 		player_health = 0
+		end_game()
 	else:
 		player_health -= health
 
 func end_game() -> void:
-	pass
+	get_node("game_over_audio").play()
+	end_scene = true
+	
